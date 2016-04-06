90c65fdecca3a6a0861759338a0a9f99a5b81cef
Y.Petremann <y.petremann@gmail.com>
2016-04-06 06:11:55 +0200

    Add option to not use flatland

--- control.lua
+++ control.lua
@@ -7,6 +7,7 @@ function bootstrap(event)
     local window = player.gui.center.add{type="frame", caption="Flatland : Which material as floor ?", name="flatland_window",direction="horizontal"}
     window.add{type="button", caption="Grass", name="flatland_choose{'grass'}"}
     window.add{type="button", caption="Concrete", name="flatland_choose{'concrete'}"}
+    window.add{type="button", caption="Don't use", name="flatland_choose{false}"}
   end
 end
 script.on_init(bootstrap)
@@ -20,13 +21,13 @@ script.on_event(defines.events.on_player_created, function(event)
     local window = player.gui.center.add{type="frame", caption="Flatland : Which material as floor ?", name="flatland_window",direction="horizontal"}
     window.add{type="button", caption="Grass", name="flatland_choose{'grass'}"}
     window.add{type="button", caption="Concrete", name="flatland_choose{'concrete'}"}
+    window.add{type="button", caption="Concrete", name="flatland_choose{''}"}
   end
 end)
 function on_gui_click_flatland_choose(event, params) 
   global.material = params[1]
   local player = game.players[event.player_index]
   player.gui.center.flatland_window.destroy()
-  print_all(global.material)
   chunk_flush()
 end
 
@@ -34,27 +35,29 @@ function chunk_flush()
   if global.material then
     while #global.chunks > 0 do
       chunk = table.remove(global.chunks, 1)
-      for key, entity in pairs(chunk.surface.find_entities_filtered({area=chunk.area, type="decorative"})) do entity.destroy() end
-      for key, entity in pairs(chunk.surface.find_entities_filtered({area=chunk.area, name="dead-dry-hairy-tree"})) do entity.destroy() end
-      for key, entity in pairs(chunk.surface.find_entities_filtered({area=chunk.area, name="dead-grey-trunk"})) do entity.destroy() end
-      for key, entity in pairs(chunk.surface.find_entities_filtered({area=chunk.area, name="dead-tree"})) do entity.destroy() end
-      for key, entity in pairs(chunk.surface.find_entities_filtered({area=chunk.area, name="dry-hairy-tree"})) do entity.destroy() end
-      for key, entity in pairs(chunk.surface.find_entities_filtered({area=chunk.area, name="dry-tree"})) do entity.destroy() end
-      for key, entity in pairs(chunk.surface.find_entities_filtered({area=chunk.area, name="stone-rock"})) do entity.destroy() end
-      local array = {}
-      for x=chunk.area.left_top.x, chunk.area.right_bottom.x, 1 do
-        for y=chunk.area.left_top.y, chunk.area.right_bottom.y, 1 do
-          tile = chunk.surface.get_tile(x, y)
-          if not (tile and tile.valid) then
-          elseif tile.name == 'out-of-map' then      
-          elseif tile.name == 'water' or tile.name == 'water-green' or tile.name == 'deepwater' or tile.name == 'deepwater-green' then
-            table.insert(array, {name="water",position={x,y}})
-          else
-            table.insert(array, {name=global.material,position={x,y}})
+      if #global.material > 0 then
+        for key, entity in pairs(chunk.surface.find_entities_filtered({area=chunk.area, type="decorative"})) do entity.destroy() end
+        for key, entity in pairs(chunk.surface.find_entities_filtered({area=chunk.area, name="dead-dry-hairy-tree"})) do entity.destroy() end
+        for key, entity in pairs(chunk.surface.find_entities_filtered({area=chunk.area, name="dead-grey-trunk"})) do entity.destroy() end
+        for key, entity in pairs(chunk.surface.find_entities_filtered({area=chunk.area, name="dead-tree"})) do entity.destroy() end
+        for key, entity in pairs(chunk.surface.find_entities_filtered({area=chunk.area, name="dry-hairy-tree"})) do entity.destroy() end
+        for key, entity in pairs(chunk.surface.find_entities_filtered({area=chunk.area, name="dry-tree"})) do entity.destroy() end
+        for key, entity in pairs(chunk.surface.find_entities_filtered({area=chunk.area, name="stone-rock"})) do entity.destroy() end
+        local array = {}
+        for x=chunk.area.left_top.x, chunk.area.right_bottom.x, 1 do
+          for y=chunk.area.left_top.y, chunk.area.right_bottom.y, 1 do
+            tile = chunk.surface.get_tile(x, y)
+            if not (tile and tile.valid) then
+            elseif tile.name == 'out-of-map' then      
+            elseif tile.name == 'water' or tile.name == 'water-green' or tile.name == 'deepwater' or tile.name == 'deepwater-green' then
+              table.insert(array, {name="water",position={x,y}})
+            else
+              table.insert(array, {name=global.material,position={x,y}})
+            end
           end
         end
+        chunk.surface.set_tiles(array)
       end
-      chunk.surface.set_tiles(array)
     end
   end
 end
